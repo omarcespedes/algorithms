70fd85a9228f122c6c09e4f165545e54afcc5f68
omarcespedes <omarcspds@gmail.com>
2017-10-02 12:29:08 -0400

    Slight improvement

--- java/src/uva/uva11547/uva11547.java
+++ java/src/uva/uva11547/uva11547.java
@@ -3,11 +3,12 @@ import java.util.Scanner;
 class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        while(sc.hasNextInt()) {
-            int n = sc.nextInt();
-            long res = 315*n + 36962;
-            //System.out.println(res);
-            System.out.println((res/10)%10);
+        int n = sc.nextInt();
+        for(int i = 0 ; i < n ; i++) {
+            int x = sc.nextInt();
+            long res = 315*x + 36962;
+            String resX = "" + res;
+            System.out.println(resX.charAt(resX.length()-2));
         }
     }
 }
