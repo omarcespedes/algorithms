9346ad768e7e209627e922c734984789e3dee00f
omarcespedes <omarcspds@gmail.com>
2017-10-17 17:52:04 -0400

    Missing some cases for but better than before

--- java/src/spoj/knapsack/knapsack.java
+++ java/src/spoj/knapsack/knapsack.java
@@ -1,10 +1,13 @@
 import java.util.Scanner;
 
-class Main {
-    static int [] arr;
-    static int [] vals;
+class Main88 {
+    static int [] arr  = new int[]{3,2,1,2,2};
+    static int [] vals = new int[]{0,4,8,5,3};
     static int n;
     public static void main(String[] args) {
+        n = 5;
+        findMax(4, 0);
+
         Scanner sc = new Scanner(System.in);
 
         int c = sc.nextInt();
@@ -18,34 +21,30 @@ class Main {
             vals[i] = sc.nextInt();
         }
 
-        long max = findMax(c, 0, 0);
+        long max = findMax(c, 0);
         System.out.println(max);
     }
 
-    static long findMax(int capacity, int i, long max) {
+    static long findMax(int capacity, int i) {
         if(i >= n - 1) {
-            return max;
+            return 0;
         }
+        int val = vals[i];
         if(arr[i] > capacity){
             if(capacity > 0){
-                return Math.max(
-                        max,
-                        findMax(capacity, i + 1, max)
-                );
+                return findMax(capacity, i + 1);
             } else {
-                return max;
+                return 0;
             }
         }
 
-        return Math.max(
-            findMax(capacity - arr[i], i+1, max + vals[i]),
-            findMax(capacity, i+1, max)
-        );
-    }
-
-    static void printArray(int [] arr) {
-        for(int i = 0 ; i < arr.length ; i++) {
-            System.out.println(arr[i]);
+        if(capacity - arr[i] == 0) {
+            return val;
         }
+
+        return Math.max(
+            val + findMax(capacity - arr[i], i+1),
+            findMax(capacity, i+1)
+        );
     }
 }
